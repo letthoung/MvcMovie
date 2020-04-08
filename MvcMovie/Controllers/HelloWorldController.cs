0041750ef5d3e61d8c3f7948bb5aa09a34a7961e
Thanh Le <tle@2ndsource.com>
2020-04-08 13:58:30 -0400

    first commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/HelloWorld/Index.cshtml");
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes: {numTimes}");
+        }
+    }
+}
